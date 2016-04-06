cbd3d3c85939152c2d326bb1e59066cef1532c46
yksz <yksz.dev@gmail.com>
2016-04-06 22:20:25 +0900

    Write the document of loggerconf.h

--- src/loggerconf.h
+++ src/loggerconf.h
@@ -5,6 +5,23 @@
 extern "C" {
 #endif /* __cplusplus */
 
+/**
+ * Configure the logger with a configuration file.
+ * If the filename is NULL, return without doing anything.
+ *
+ * The following is the configurable key/value list.
+ * |key                        |value                                       |
+ * |:--------------------------|:-------------------------------------------|
+ * |level                      |TRACE, DEBUG, INFO, WARN, ERROR or FATAL    |
+ * |logger                     |console or file                             |
+ * |logger.console.output      |stdout or stderr                            |
+ * |logger.file.filename       |A output filename (max length is 255 bytes) |
+ * |logger.file.maxFileSize    |1-LONG_MAX [bytes] or 1 MB (size <= 0)      |
+ * |logger.file.maxBackupFiles |0-255                                       |
+ *
+ * @param[in] filename The name of the configuration file
+ * @return Non-zero value upon success or 0 on error
+ */
 int logger_configure(const char* filename);
 
 #ifdef __cplusplus
