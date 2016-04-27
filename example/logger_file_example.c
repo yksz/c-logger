28cf557d36c9ffdd41764b17a1bb595ff9731003
yksz <yksz.dev@gmail.com>
2016-04-27 15:48:50 +0900

    example: Enable to build with CMake

--- example/logger_file_example.c
+++ example/logger_file_example.c
@@ -1,9 +1,9 @@
 #include "logger.h"
 
 int main(void) {
-    logger_initFileLogger("logs/log.txt", 1024 * 1024, 5);
+    logger_initFileLogger("log.txt", 1024 * 1024, 5);
     logger_setLevel(LogLevel_DEBUG);
-    LOG_INFO("file logger");
+    LOG_INFO("file logging");
     LOG_DEBUG("format example: %d%c%s", 1, '2', "3");
     return 0;
 }
---
+++ example/loggerconf_example.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "logger.h"
+#include "loggerconf.h"
+
+int main(int argc, char* argv[]) {
+    char filename[256] = "logger.conf";
+    if (argc <= 1) {
+        printf("usage: %s <conf file>\n", argv[0]);
+        return 1;
+    }
+    strncpy(filename, argv[1], strlen(argv[1]));
+
+    logger_configure(filename);
+    LOG_TRACE("trace");
+    LOG_DEBUG("degug");
+    LOG_INFO("info");
+    LOG_WARN("warn");
+    LOG_ERROR("error");
+    LOG_FATAL("fatal");
+    return 0;
+}
